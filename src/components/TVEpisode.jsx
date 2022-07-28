6bb6fb854f1a3c8ee8758914233fa018bb844d15
Argo Nurcahyo <argonurcahyo25@gmail.com>
2022-07-28 09:35:26 +0700

    tv episode name fix

--- src/components/TVEpisode.jsx
+++ src/components/TVEpisode.jsx
@@ -3,29 +3,29 @@ import tmdb from '../apis/tmdb';
 
 const TVEpisode = ({ tvId, seasonId, episodeId }) => {
 
- const [tvEpisode, setTvEpisode] = useState({})
+    const [tvEpisode, setTvEpisode] = useState({})
 
- const fetchTvEpisode = async (tvId, seasonId, episodeId) => {
-  try {
-   const fetchData = await tmdb.get(`tv/${tvId}/season/${seasonId}/episode/${episodeId}`);
-   setTvEpisode(fetchData.data);
-   // console.log(fetchData.data)
-  } catch (error) {
-   console.log(error)
-   setTvEpisode("");
-  }
- }
+    const fetchTvEpisode = async (tvId, seasonId, episodeId) => {
+        try {
+            const fetchData = await tmdb.get(`tv/${tvId}/season/${seasonId}/episode/${episodeId}`);
+            setTvEpisode(fetchData.data);
+            // console.log(fetchData.data)
+        } catch (error) {
+            console.log(error)
+            setTvEpisode("");
+        }
+    }
 
- useEffect(() => {
-  fetchTvEpisode(tvId, seasonId, episodeId);
- }, [tvId, seasonId, episodeId]);
+    useEffect(() => {
+        fetchTvEpisode(tvId, seasonId, episodeId);
+    }, [tvId, seasonId, episodeId]);
 
- return (
-  <div>
-   S{seasonId.toString().padStart(2, "0")}E{episodeId.toString().padStart(2, "0")}. {tvEpisode.name}
-   {/* <img src={`${BASE_IMG_URL}${tvEpisode.still_path}`} alt={tvEpisode.id} /> */}
-  </div>
- )
+    return (
+        <div>
+            S{seasonId?.toString().padStart(2, "0")}E{episodeId?.toString().padStart(2, "0")}. {tvEpisode?.name}
+            {/* <img src={`${BASE_IMG_URL}${tvEpisode.still_path}`} alt={tvEpisode.id} /> */}
+        </div>
+    )
 }
 
 export default TVEpisode
